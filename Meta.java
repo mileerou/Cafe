ccee6ef00bc73a02b949ad5739bb4aa66c58a17d
JuniorLancerio <juniorlancerio77@gmail.com>
2025-09-11 20:18:27 -0600

    Nuevo avance clase Meta

--- Meta.java
+++ Meta.java
@@ -1,6 +1,8 @@
 public class Meta {
-    private String descripcion; private boolean completada;
-    public Meta(String descripcion) { this.descripcion = descripcion; }
-    public String toString() { return descripcion + (completada ? " ✔" : " ✘"); }
+    private String id, descripcion; private boolean completada; private int recompensa;
+    public Meta(String id, String descripcion, int recompensa) {
+        this.id = id; this.descripcion = descripcion; this.recompensa = recompensa; this.completada = false;
+    }
+    public void completar() { completada = true; }
+    public String toString() { return descripcion + " (" + (completada ? "✔" : "Pendiente") + ")"; }
 }
-

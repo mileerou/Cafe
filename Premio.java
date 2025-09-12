6a03792b314fd7e7c0783bb9e36e81505d086cd3
JuniorLancerio <juniorlancerio77@gmail.com>
2025-09-11 20:19:09 -0600

    Primer avance clase Premio

--- Premio.java
+++ Premio.java
@@ -1,5 +1,8 @@
 public class Premio {
-    private String nombre; private int puntos;
-    public Premio(String nombre, int puntos) { this.nombre = nombre; this.puntos = puntos; }
-    public String toString() { return nombre + " (" + puntos + " pts)"; }
+    private String id, nombre; private int puntosRequeridos, stock;
+    public Premio(String id, String nombre, int puntosRequeridos, int stock) {
+        this.id = id; this.nombre = nombre; this.puntosRequeridos = puntosRequeridos; this.stock = stock;
+    }
+    public boolean isDisponible() { return stock > 0; }
+    public String toString() { return nombre + " - " + puntosRequeridos + " pts"; }
 }

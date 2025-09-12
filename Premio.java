7f6a60866c5aec3d722595165cd69f4cc1121653
JuniorLancerio <juniorlancerio77@gmail.com>
2025-09-11 20:23:57 -0600

    Clase premio terminada.

--- Premio.java
+++ Premio.java
@@ -1,8 +1,23 @@
 public class Premio {
-    private String id, nombre; private int puntosRequeridos, stock;
-    public Premio(String id, String nombre, int puntosRequeridos, int stock) {
-        this.id = id; this.nombre = nombre; this.puntosRequeridos = puntosRequeridos; this.stock = stock;
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private int puntosRequeridos;
+    private int stock;
+
+    public Premio(String id, String nombre, String descripcion, int puntosRequeridos, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.puntosRequeridos = puntosRequeridos;
+        this.stock = stock;
     }
-    public boolean isDisponible() { return stock > 0; }
-    public String toString() { return nombre + " - " + puntosRequeridos + " pts"; }
+
+    public boolean isDisponible() {
+        return stock > 0;
+    }
+
+    public String toString() {
+        return nombre + " (" + puntosRequeridos + " pts) - Stock: " + stock;
+    }
 }

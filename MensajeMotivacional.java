e17098c65c59ea438114ec1cb1a9a97f4b6d2cff
JuniorLancerio <juniorlancerio77@gmail.com>
2025-09-11 20:16:16 -0600

    Base de la clase MensajeMotivacional

---
+++ MensajeMotivacional.java
@@ -0,0 +1,5 @@
+public class MensajeMotivacional {
+    private String contenido;
+    public MensajeMotivacional(String contenido) { this.contenido = contenido; }
+    public String toString() { return "💡 " + contenido; }
+}

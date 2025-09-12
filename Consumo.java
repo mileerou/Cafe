4eb9bc2f0cf3d9a68437d97833b836844c05b6e5
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2025-09-11 21:25:01 -0600

    Terminada la clase modelo Consumo con getters, setters y override.

--- Consumo.java
+++ Consumo.java
@@ -17,4 +17,67 @@ public class Consumo {
         this.tipoCafe = tipoCafe;
         this.respuestasExtras = respuestasExtras;
     }
+
+    // Getters
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getTamañoTaza() {
+        return tamanoTaza;
+    }
+
+    public String getTipoAzucar() {
+        return tipoAzucar;
+    }
+
+    public String getTipoLeche() {
+        return tipoLeche;
+    }
+
+    public String getTipoCafe() {
+        return tipoCafe;
+    }
+
+    public String getRespuestasExtras() {
+        return respuestasExtras;
+    }
+
+    // Setters
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setTamañoTaza(String tamañoTaza) {
+        this.tamanoTaza = tamañoTaza;
+    }
+
+    public void setTipoAzucar(String tipoAzucar) {
+        this.tipoAzucar = tipoAzucar;
+    }
+
+    public void setTipoLeche(String tipoLeche) {
+        this.tipoLeche = tipoLeche;
+    }
+
+    public void setTipoCafe(String tipoCafe) {
+        this.tipoCafe = tipoCafe;
+    }
+
+    public void setRespuestasExtras(String respuestasExtras) {
+        this.respuestasExtras = respuestasExtras;
+    }
+
+    @Override
+    public String toString() {
+        return "Consumo{" +
+                "fecha=" + fecha +
+                ", tamañoTaza='" + tamanoTaza + '\'' +
+                ", tipoAzucar='" + tipoAzucar + '\'' +
+                ", tipoLeche='" + tipoLeche + '\'' +
+                ", tipoCafe='" + tipoCafe + '\'' +
+                ", respuestasExtras='" + respuestasExtras + '\'' +
+                '}';
+    }
 }
